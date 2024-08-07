2541a3c36e989f7ff1ed97ed64b58d027c576581
Matt Sharp <msharp19@users.noreply.github.com>
2024-08-07 19:21:19 +0800

    Update kaiju-finance-stake-lockup.sol

--- contracts/kaiju-finance-stake-lockup.sol
+++ contracts/kaiju-finance-stake-lockup.sol
@@ -5,33 +5,75 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract KaijuFinanceStakeLockup is Ownable, ReentrancyGuard
+interface IMintableToken is IERC20 {
+    function mint(address user, uint256 amount) external;
+}
+
+interface ICreditLine {
+    function getCollateralAmount(address sender) external returns(uint256);
+}
+
+contract KaijuFinanceStakeLockup is Ownable, ReentrancyGuard 
 {
+    //using SafeMath for uint256;
+
     struct Stake {
        uint256 Id;
+       address Owner;
        uint256 AmountStaked;
-       uint256 AmountToReceive;   
        uint256 CreatedAt;
-       uint256 CanCollectAt;
-       uint256 CollectedAt;
        bool Active;
+    }
+
+    struct WithdrawnStake {
+       uint256 Id;
+       address Owner;
+       uint256 AmountWithdrawn;
+       uint256 CreatedAt;
+       bool Active;
+    }
+
+    struct SupportedToken {
+       string Name;
+       string Symbol;
+       address Address;
+       uint256 TokenRate;
+       bool Active;
+    }
+
+    struct TotalActiveStake {
+        uint256 Value;
     }
 
     uint256 _minimumStakeAmount = 1000000000000000;
     uint256 _earnRate = 1;
     uint256 _currentStakeId = 1;
-    uint256 _lockupTime = 10000;
+    uint256 _currentWithdrawnStakeId = 1;
 
-    mapping(address => Stake[]) _allUsersStakes;
+    Stake[] _allStakes;
+    WithdrawnStake[] _allWithdrawnStakes;
 
-    IERC20 _kaijuFinanceToken;
+    mapping(address => uint256[]) _allUsersStakes;
+    mapping(address => uint256[]) _allUsersWithdrawnStakes;
+    mapping(address => uint256) _usersCurrentStakeTotals;
 
-    constructor(address kaijuFinanceTokenAddress){
-        _kaijuFinanceToken = IERC20(kaijuFinanceTokenAddress);
+    mapping(address => SupportedToken) _supportedTokens;
+
+    IERC20 _kaijuFinanceLiquidStakingToken;
+    ICreditLine _kaijuFinanceCreditLine;
+
+    constructor(address kaijuFinanceLiquidStakingTokenAddress, address kaijuFinanceCreditLineAddress){
+        _kaijuFinanceLiquidStakingToken = IERC20(kaijuFinanceLiquidStakingTokenAddress);
+        _kaijuFinanceCreditLine = ICreditLine(kaijuFinanceCreditLineAddress);
     }
 
     event EthStaked(uint256 indexed Id, address indexed user, uint256 AmountStaked, uint256 AmountToReceive, uint256 CreatedAt);
     event StakeCollected(uint256 indexed Id, address indexed user, uint256 AmountReceived, uint256 CollectedAt);
+
+    function updateSupportedToken(string memory name, string memory symbol, address contractAddress) external onlyOwner nonReentrant{
+        SupportedToken memory supportedToken = SupportedToken(name, symbol, contractAddress, 1, true);
+        _supportedTokens[contractAddress].Name = name;
+    }
  
     function stake() external payable nonReentrant{       
         // Validate stake amount
@@ -40,56 +82,49 @@ contract KaijuFinanceStakeLockup is Ownable, ReentrancyGuard
         // Calculate the amount to recieve after lockup
         uint256 amountToReceive = msg.value * _earnRate;
 
-        // Calculate when to collect
-        uint256 canCollectAt = block.timestamp + _lockupTime;
-    
         // Create new stake record
-        Stake memory newStake = Stake(_currentStakeId++, msg.value, amountToReceive, block.timestamp, canCollectAt, 0, true);
-        _allUsersStakes[msg.sender].push(newStake);
+        Stake memory newStake = Stake(_currentStakeId++, msg.sender, msg.value, block.timestamp, true);
+        _allStakes.push(newStake);
+        uint256 newStakeIndex = _allStakes.length-1;
+        _allUsersStakes[msg.sender].push(newStakeIndex);
+        uint256 usersCurrentStakeTotal = _usersCurrentStakeTotals[msg.sender];
+        _usersCurrentStakeTotals[msg.sender] = usersCurrentStakeTotal + msg.value;
 
         // TODO: Reference the kaiju token contract definition NOT IERC20
-        _kaijuFinanceToken.mint(msg.sender, amountToReceive);
+        _kaijuFinanceLiquidStakingToken.mint(msg.sender, amountToReceive);
 
         emit EthStaked(newStake.Id, msg.sender, msg.value, amountToReceive, block.timestamp);
     }
 
-    function withdrawStake(uint256 id) external nonReentrant {
-        // Get all the users stakes
-        Stake[] memory userStakes = _allUsersStakes[msg.sender];
+    function withdrawStake(uint256 amount) external nonReentrant {
 
-        // Try to find the stake with the id specified
-        for (uint256 i = 0; i<userStakes.length; i++) 
-        {
-            // Get the iterations stake
-            Stake memory userStake = userStakes[i];
+        uint256 currentStakeAmount = _usersCurrentStakeTotals[msg.sender];
 
-            // Check if the id supplied matches
-            if(userStake.Id == id)
-            {
-                // Ensure its now time to collect
-                require(userStake.CanCollectAt <= block.timestamp, 'Please wait until the lockup period has finished to collect');
+        // Ensure credit isnt already on loan
+        uint256 amountRequiredForCollateral = _kaijuFinanceCreditLine.getCollateralAmount(msg.sender);
+        require((currentStakeAmount - amountRequiredForCollateral) > amount, 'The withdraw will reduce the collateral below what is required since there is an active loan. Please try a lower amount');
 
-                // Ensure it has not already been collected
-                require(userStake.CollectedAt == 0, '');
+        // Ensure contract has enough to honor the withdraw
+         require(address(this).balance >= amount, 'The contract needs additional funding before this can be completed');
 
-                // Ensure contract has enough to honor the withdraw
-                require(address(this).balance >= userStake.AmountToReceive, 'The contract needs additional funding before this can be completed');
-
-                // Ensure can collect tokens
-                require(_kaijuFinanceToken.allowance(msg.sender, address(this)) == userStake.AmountToReceive, 'Please approve the exact amount of tokens required');
+        // Ensure can collect tokens
+        require(_kaijuFinanceLiquidStakingToken.allowance(msg.sender, address(this)) == amount, 'Please approve the exact amount of tokens required');
  
-                // Collect
-                _kaijuFinanceToken.safeTransferFrom(msg.sender, address(this), userStake.AmountToReceive);
+        // Collect
+        _kaijuFinanceLiquidStakingToken.safeTransferFrom(msg.sender, address(this), amount);
 
-                // Mark as collected
-                userStake.CollectedAt = block.timestamp;
-                userStake.Active = false;
+        // Mark as collected
+        _usersCurrentStakeTotals[msg.sender] = (currentStakeAmount - amountRequiredForCollateral);
+        
+        // Create a record
+        WithdrawnStake memory stakeWithdrawal = WithdrawnStake(_currentWithdrawnStakeId++, msg.sender, amount, block.timestamp, true);
+        _allWithdrawnStakes.push(stakeWithdrawal);
+        uint256 newWithdrawnStakeIndex = _allUsersWithdrawnStakes.length-1;
+        _allUsersWithdrawnStakes[msg.sender].push(newWithdrawnStakeIndex);
 
-                // Send back value
-                payable(msg.sender).transfer(userStake.AmountToReceive);
+        // Send back value
+        payable(msg.sender).transfer(amount);
 
-                emit StakeCollected(userStake.Id, msg.sender, userStake.AmountToReceive, block.timestamp);
-            }
-        }
+        emit StakeCollected(stakeWithdrawal.Id, msg.sender, amount, block.timestamp);
     }
 }
