5ee4ac3403c1dd4b4b06d4d2bde7b86cd110a29d
Matt Sharp <msharp19@users.noreply.github.com>
2024-08-08 15:47:06 +0800

    Update IKaijuFinanceCreditLine.sol

---
+++ contracts/interfaces/IKaijuFinanceCreditLine.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity =0.8.12.0;
+
+interface IKaijuFinanceCreditLine {
+    function getRequiredCollateralAmount(address user) external returns(uint256);
+}
