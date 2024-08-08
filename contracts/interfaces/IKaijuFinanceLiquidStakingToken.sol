1150e87d9fd0f626fa175979a32738fff0d0613a
Matt Sharp <msharp19@users.noreply.github.com>
2024-08-08 16:49:41 +0800

    Update IKaijuFinanceLiquidStakingToken.sol

--- contracts/interfaces/IKaijuFinanceLiquidStakingToken.sol
+++ contracts/interfaces/IKaijuFinanceLiquidStakingToken.sol
@@ -5,5 +5,5 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IKaijuFinanceLiquidStakingToken is IERC20 {
     function mint(address user, uint256 amount) external;
-    function burn(address from, uint256 value) external returns (bool);
+    function burn(address from, uint256 value) external;
 }
