21bca6eebb028a5e2e941c4c5e367b1b27659cb7
Matt Sharp <msharp19@users.noreply.github.com>
2024-08-08 14:11:50 +0800

    Update kaiju-finance-token.sol

--- contracts/kaiju-finance-token.sol
+++ contracts/kaiju-finance-token.sol
@@ -6,12 +6,16 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract KaijuFinanceToken is IERC20, ERC20, Ownable, ReentrancyGuard
+contract KaijuFinanceLiquidToken is IERC20, ERC20, Ownable, ReentrancyGuard
 {
-    constructor () ERC20('Kaiju Finance Token','CST'){
+    constructor () ERC20('Kaiju Finance Liquid Token','KFLT'){
     }
 
     function mint(address user, uint256 amount) public onlyOwner nonReentrant{
         _mint(user, amount);
+    }
+
+    function burn(uint256 amount) public onlyOwner nonReentrant{
+        _burn(amount);
     }
 }
