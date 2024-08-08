b9a4be644b304ee2d3dae7a594556ed7ecf871c3
Matt Sharp <msharp19@users.noreply.github.com>
2024-08-08 15:53:43 +0800

    Update IKaijuFinanceLiquidStakingToken.sol

--- contracts/interfaces/IKaijuFinanceLiquidStakingToken.sol
+++ contracts/interfaces/IKaijuFinanceLiquidStakingToken.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity =0.8.12.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 interface IKaijuFinanceLiquidStakingToken is IERC20 {
     function mint(address user, uint256 amount) external;
     function burn(address from, uint256 value) external returns (bool);
